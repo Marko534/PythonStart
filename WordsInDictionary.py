33063b48a3811eea8ff35b3b5eb9b291fda2b5fa
Vistinska Licnost <TotalnoVistinkiMail@pp.mk>
2022-07-21 14:02:43 +0200

    Zborovi i koj se najcesto se pojavuva

--- WordsInDictionary.py
+++ WordsInDictionary.py
@@ -5,11 +5,27 @@ Created on Thu Jul 21 13:18:18 2022
 @author: marko.ilioski
 """
 
+def NumberOfElementsList( element, *list):
+    Number = 0
+    for i in words:
+        if i == element:
+            Number = Number +1
+    return Number
+
+
+
 words = [   'red', 'green', 'black', 'pink', 'black', 'white', 'black', 'eyes',
    'white', 'black', 'orange', 'pink', 'pink', 'red', 'red', 'white', 'orange',
    'white', "black", 'pink', 'green', 'green', 'pink', 'green', 'pink',
    'white', 'orange', "orange", 'red']
 
-print (words)
 
-#aaaaaaaaaaa
+print (words)
+final = {}
+for i in words:
+    if i not in final:
+        final.update({i: NumberOfElementsList(i, words)})
+        
+final = sorted(final.items(), key=lambda x: x[1])
+print (final)
+
