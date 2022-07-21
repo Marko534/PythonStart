7298dc5bb15a2264f6cbdffd174fc7641ba8518f
Vistinska Licnost <TotalnoVistinkiMail@pp.mk>
2022-07-21 16:29:23 +0200

    Da vidam now txt file kako odi

---
+++ AlterTextFile.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 21 16:18:25 2022
+
+@author: marko.ilioski
+"""
+
+Text = open ('PrvMojFile.txt', 'a')
+Text.write('Uste ne znam za binary trea \n')
+
+Text.close()
