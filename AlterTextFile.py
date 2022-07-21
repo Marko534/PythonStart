c4b7599289658eb856709ee7a17fd4aac015ed0b
Vistinska Licnost <TotalnoVistinkiMail@pp.mk>
2022-07-21 16:32:32 +0200

    Zaborajv da citam

--- AlterTextFile.py
+++ AlterTextFile.py
@@ -6,6 +6,9 @@ Created on Thu Jul 21 16:18:25 2022
 """
 
 Text = open ('PrvMojFile.txt', 'a')
-Text.write('Uste ne znam za binary trea \n')
+Text.write('FFFFFFFF \n')
 
 Text.close()
+
+Text = open ('PrvMojFile.txt', 'r')
+print (Text.read())
